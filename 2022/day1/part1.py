77cae065ada6a2841b3cab162c4bb6ed6acd3d08
Grozby <gabriele.aldeghi.27@gmail.com>
2022-12-05 23:48:30 +0000

    feat(day1,2022): compute max directly at file reading

--- 2022/day1/part1.py
+++ 2022/day1/part1.py
@@ -2,13 +2,15 @@ from typing import List
 
 if __name__ == "__main__":
     elfs: List[List[int]] = [[]]
+    max_elf = 0
     with open("./input", "r") as f:
         index = 0
         while line := f.readline():
             if line.rstrip() == "":
+                max_elf = max(max_elf, sum(elfs[index]))
                 index += 1
                 elfs.append([])
             else:
                 elfs[index].append(int(line))
 
-    print(max([sum(e) for e in elfs]))
+    print(max_elf)
